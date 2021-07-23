f141fef7ad10b49aa12f38d10c7a69068625309e
Kevin Moore <kevmoo@users.noreply.github.com>
2021-07-23 12:26:50 -0700

    Bump dependency on json_serializable (#72)

--- lib/src/dependency.g.dart
+++ lib/src/dependency.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: deprecated_member_use_from_same_package, lines_longer_than_80_chars, prefer_expression_function_bodies
+// ignore_for_file: deprecated_member_use_from_same_package, unnecessary_cast
 
 part of 'dependency.dart';
 
@@ -8,51 +8,64 @@ part of 'dependency.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-SdkDependency _$SdkDependencyFromJson(Map json) {
-  return $checkedNew('SdkDependency', json, () {
-    final val = SdkDependency(
-      $checkedConvert(json, 'sdk', (v) => v as String),
-      version: $checkedConvert(
-          json, 'version', (v) => _constraintFromString(v as String?)),
+SdkDependency _$SdkDependencyFromJson(Map json) => $checkedCreate(
+      'SdkDependency',
+      json,
+      ($checkedConvert) {
+        final val = SdkDependency(
+          $checkedConvert('sdk', (v) => v as String),
+          version: $checkedConvert(
+              'version', (v) => _constraintFromString(v as String?)),
+        );
+        return val;
+      },
     );
-    return val;
-  });
-}
 
-GitDependency _$GitDependencyFromJson(Map json) {
-  return $checkedNew('GitDependency', json, () {
-    final val = GitDependency(
-      $checkedConvert(json, 'url', (v) => parseGitUri(v as String)),
-      ref: $checkedConvert(json, 'ref', (v) => v as String?),
-      path: $checkedConvert(json, 'path', (v) => v as String?),
+GitDependency _$GitDependencyFromJson(Map json) => $checkedCreate(
+      'GitDependency',
+      json,
+      ($checkedConvert) {
+        final val = GitDependency(
+          $checkedConvert('url', (v) => parseGitUri(v as String)),
+          ref: $checkedConvert('ref', (v) => v as String?),
+          path: $checkedConvert('path', (v) => v as String?),
+        );
+        return val;
+      },
     );
-    return val;
-  });
-}
 
-HostedDependency _$HostedDependencyFromJson(Map json) {
-  return $checkedNew('HostedDependency', json, () {
-    $checkKeys(json,
-        allowedKeys: const ['version', 'hosted'],
-        disallowNullValues: const ['hosted']);
-    final val = HostedDependency(
-      version: $checkedConvert(
-          json, 'version', (v) => _constraintFromString(v as String?)),
-      hosted: $checkedConvert(json, 'hosted',
-          (v) => v == null ? null : HostedDetails.fromJson(v as Object)),
+HostedDependency _$HostedDependencyFromJson(Map json) => $checkedCreate(
+      'HostedDependency',
+      json,
+      ($checkedConvert) {
+        $checkKeys(
+          json,
+          allowedKeys: const ['version', 'hosted'],
+          disallowNullValues: const ['hosted'],
+        );
+        final val = HostedDependency(
+          version: $checkedConvert(
+              'version', (v) => _constraintFromString(v as String?)),
+          hosted: $checkedConvert('hosted',
+              (v) => v == null ? null : HostedDetails.fromJson(v as Object)),
+        );
+        return val;
+      },
     );
-    return val;
-  });
-}
 
-HostedDetails _$HostedDetailsFromJson(Map json) {
-  return $checkedNew('HostedDetails', json, () {
-    $checkKeys(json,
-        allowedKeys: const ['name', 'url'], disallowNullValues: const ['url']);
-    final val = HostedDetails(
-      $checkedConvert(json, 'name', (v) => v as String),
-      $checkedConvert(json, 'url', (v) => parseGitUriOrNull(v as String?)),
+HostedDetails _$HostedDetailsFromJson(Map json) => $checkedCreate(
+      'HostedDetails',
+      json,
+      ($checkedConvert) {
+        $checkKeys(
+          json,
+          allowedKeys: const ['name', 'url'],
+          disallowNullValues: const ['url'],
+        );
+        final val = HostedDetails(
+          $checkedConvert('name', (v) => v as String),
+          $checkedConvert('url', (v) => parseGitUriOrNull(v as String?)),
+        );
+        return val;
+      },
     );
-    return val;
-  });
-}
--- lib/src/pubspec.g.dart
+++ lib/src/pubspec.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: deprecated_member_use_from_same_package, lines_longer_than_80_chars, prefer_expression_function_bodies
+// ignore_for_file: deprecated_member_use_from_same_package, unnecessary_cast
 
 part of 'pubspec.dart';
 
@@ -8,44 +8,45 @@ part of 'pubspec.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Pubspec _$PubspecFromJson(Map json) {
-  return $checkedNew('Pubspec', json, () {
-    final val = Pubspec(
-      $checkedConvert(json, 'name', (v) => v as String),
-      version: $checkedConvert(
-          json, 'version', (v) => _versionFromString(v as String?)),
-      publishTo: $checkedConvert(json, 'publish_to', (v) => v as String?),
-      author: $checkedConvert(json, 'author', (v) => v as String?),
-      authors: $checkedConvert(json, 'authors',
-          (v) => (v as List<dynamic>?)?.map((e) => e as String).toList()),
-      environment: $checkedConvert(
-          json, 'environment', (v) => _environmentMap(v as Map?)),
-      homepage: $checkedConvert(json, 'homepage', (v) => v as String?),
-      repository: $checkedConvert(
-          json, 'repository', (v) => v == null ? null : Uri.parse(v as String)),
-      issueTracker: $checkedConvert(json, 'issue_tracker',
-          (v) => v == null ? null : Uri.parse(v as String)),
-      documentation:
-          $checkedConvert(json, 'documentation', (v) => v as String?),
-      description: $checkedConvert(json, 'description', (v) => v as String?),
-      dependencies:
-          $checkedConvert(json, 'dependencies', (v) => parseDeps(v as Map?)),
-      devDependencies: $checkedConvert(
-          json, 'dev_dependencies', (v) => parseDeps(v as Map?)),
-      dependencyOverrides: $checkedConvert(
-          json, 'dependency_overrides', (v) => parseDeps(v as Map?)),
-      flutter: $checkedConvert(
-          json,
-          'flutter',
-          (v) => (v as Map?)?.map(
-                (k, e) => MapEntry(k as String, e),
-              )),
+Pubspec _$PubspecFromJson(Map json) => $checkedCreate(
+      'Pubspec',
+      json,
+      ($checkedConvert) {
+        final val = Pubspec(
+          $checkedConvert('name', (v) => v as String),
+          version: $checkedConvert(
+              'version', (v) => _versionFromString(v as String?)),
+          publishTo: $checkedConvert('publish_to', (v) => v as String?),
+          author: $checkedConvert('author', (v) => v as String?),
+          authors: $checkedConvert('authors',
+              (v) => (v as List<dynamic>?)?.map((e) => e as String).toList()),
+          environment:
+              $checkedConvert('environment', (v) => _environmentMap(v as Map?)),
+          homepage: $checkedConvert('homepage', (v) => v as String?),
+          repository: $checkedConvert(
+              'repository', (v) => v == null ? null : Uri.parse(v as String)),
+          issueTracker: $checkedConvert('issue_tracker',
+              (v) => v == null ? null : Uri.parse(v as String)),
+          documentation: $checkedConvert('documentation', (v) => v as String?),
+          description: $checkedConvert('description', (v) => v as String?),
+          dependencies:
+              $checkedConvert('dependencies', (v) => parseDeps(v as Map?)),
+          devDependencies:
+              $checkedConvert('dev_dependencies', (v) => parseDeps(v as Map?)),
+          dependencyOverrides: $checkedConvert(
+              'dependency_overrides', (v) => parseDeps(v as Map?)),
+          flutter: $checkedConvert(
+              'flutter',
+              (v) => (v as Map?)?.map(
+                    (k, e) => MapEntry(k as String, e),
+                  )),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'publishTo': 'publish_to',
+        'issueTracker': 'issue_tracker',
+        'devDependencies': 'dev_dependencies',
+        'dependencyOverrides': 'dependency_overrides'
+      },
     );
-    return val;
-  }, fieldKeyMap: const {
-    'publishTo': 'publish_to',
-    'issueTracker': 'issue_tracker',
-    'devDependencies': 'dev_dependencies',
-    'dependencyOverrides': 'dependency_overrides'
-  });
-}
