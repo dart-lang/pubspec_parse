23f8f21eb990d59d31d7a951bd7c432358f508aa
Moritz <mosum@google.com>
2023-08-31 09:50:10 +0200

    Add second dep in test

--- test/pubspec_overrides_test.dart
+++ test/pubspec_overrides_test.dart
@@ -9,13 +9,26 @@ void main() {
     const yaml = '''
 dependency_overrides:
   transmogrify: '3.2.1'
+  some_other_package:
+    git:
+      url: git@github.com:org/some_other_package
+      ref: some-branch
 ''';
     final pubspecOverrides = PubspecOverrides.parse(yaml);
-    expect(pubspecOverrides.dependencyOverrides.length, 1);
-    final dependency = pubspecOverrides.dependencyOverrides.entries.first;
-    expect(dependency.key, 'transmogrify');
-    expect(dependency.value, isA<HostedDependency>());
-    expect((dependency.value as HostedDependency).version, Version(3, 2, 1));
+    expect(pubspecOverrides.dependencyOverrides.length, 2);
+    final dependency1 = pubspecOverrides.dependencyOverrides.entries.first;
+    expect(dependency1.key, 'transmogrify');
+    expect(dependency1.value, isA<HostedDependency>());
+    expect((dependency1.value as HostedDependency).version, Version(3, 2, 1));
+
+    final dependency2 = pubspecOverrides.dependencyOverrides.entries.last;
+    expect(dependency2.key, 'some_other_package');
+    expect(dependency2.value, isA<GitDependency>());
+    expect(
+      (dependency2.value as GitDependency).url.toString(),
+      'ssh://git@github.com/org/some_other_package',
+    );
+    expect((dependency2.value as GitDependency).ref, 'some-branch');
   });
 
   test('Parsing pubspec_overrides.yaml in lenient mode', () {
